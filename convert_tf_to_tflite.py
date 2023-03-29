bfb1643951ea9ee9b5092795c45f060593efdb6e
Carina <Cococatty@users.noreply.github.com>
2023-03-30 01:30:26 +1300

    Init (#2)
    * remove commented out code

--- convert_tf_to_tflite.py
+++ convert_tf_to_tflite.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
-print(tf.__version__)
+
 
 converter = tf.lite.TFLiteConverter.from_saved_model('outputs')
 tflite_model = converter.convert()
 
-with open('outputs/yolov7_model.tflite', 'wb') as f:
+
+with open('outputs/yolov7.tflite', 'wb') as f:
   f.write(tflite_model)
